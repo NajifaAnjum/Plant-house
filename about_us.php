ddc460da0267bc77eb9061b2f39848ce8f155cc0
Najifa Anjum <41308140+NajifaAnjum@users.noreply.github.com>
2022-12-04 16:48:41 +0600

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<link href="css/page1.css" rel="stylesheet"/>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<style>
+html,body {
+	margin: 0;
+	padding: 0;
+}
+main {
+	max-width: 1400px;
+	min-width: 320px;
+	margin: 0 auto;
+}
+</style>
+<link rel="stylesheet" href="css/page1.css" class="text/css">
+<div class="header">
+   <div class="main_menu">
+     <ul>
+		<li><a href="4.admin_login.php"><i class="fas fa-user"></i>&nbsp;&nbsp;Login</a></li>
+		<!--<li><a href=""><i class="fas fa-user"></i>&nbsp;&nbsp;Customer login</a></li>
+		<li><a href=""><i class="far fa-address-book"></i>&nbsp;&nbsp;Not a member?Register</a></li>-->
+		<li><a href="">Call us now +880-01700-540523</a></li>
+	 </ul>
+	</div> 
+	
+</div>
+<div class="header2">
+   <div class="header2_left"><h1 style="font-family:tahoma; float:left; width:40%; padding-top:20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PLANT HOUSE</h1></div>
+   <div class="header2_right">
+   <ul>
+		<li><a href="7.user_page1.html">HOME</a></li>
+		<li><a href="8.user_category.php">CATEGORIES</a></li>
+		<li><a href="about_us.php">ABOUT US</a></li>
+		<li><a href="rating.php">RATE US</a></li>
+		<!--<li><a href="">BLOG</a></li>-->
+		
+	 </ul>
+   </div>
+</div>
+<div class="footer">
+			
+			<div class="footer_top">
+				
+				<div class="footer_two">
+				<ul>
+						<li style="color:white; background:#1D2120; font-weight:bold;"><a href="" class="active">&nbsp;&nbsp;&nbsp;About Us</a></li>
+						<li><a href="" >&nbsp;&nbsp;&nbsp;We are a supplier, importer, and exporter of plants.   </a></li>
+						<li><a href="">&nbsp;&nbsp;&nbsp;Fruit plants, flower plants, spice plants, herbal plants, seeds,</a></li>
+						<li><a href="">&nbsp;&nbsp;&nbsp;and bonsai of the highest quality are delivered throughout Bangladesh</a></li>
+						<li><a href="">&nbsp;&nbsp;&nbsp;by Plant House. We always offer the greatest customer service.</a></li>
+						
+					</ul>
+				</div>
+				
+				<div class="footer_four">
+					<ul>
+						<li style="color:white; background:#1D2120; font-weight:bold;"><a href="" class="active">&nbsp;&nbsp;&nbsp;Contact Us</a></li>
+						<li><a href="" >&nbsp;&nbsp;&nbsp;For any queries: 01700540523</a></li>
+						<li><a href="">&nbsp;&nbsp;&nbsp;Any Information: 01700540523</a></li>
+						<li><a href="">&nbsp;&nbsp;&nbsp;Email: afrinarashidbadhon@gmail.com</a></li>
+						<li><a href="">&nbsp;&nbsp;&nbsp;WhatsApp: 01700540523</a></li>
+						<li><a href="https://www.facebook.com/afrina.niloy?mibextid=ZbWKwL">&nbsp;&nbsp;&nbsp;Facebook: Afrina Rashid  </a></li>
+					</ul>
+				</div>
+				
+			</div>
+			<div class="footer_bottom">
+			   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Copyright &copy; 2022 | Your content items | CSS templates.
+			</div>
+		</div>
+
+</body>
+</html>
